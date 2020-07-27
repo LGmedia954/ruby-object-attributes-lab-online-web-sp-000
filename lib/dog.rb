29145c2d1659af715b5e445ec1be4f2730c03012
Leslie G <lesliegonzalez@me.com>
2020-07-27 20:13:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,13 +3,13 @@ class Dog
   def initialize(name)
     @name = name
   end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
   
   def name
     @name
-  end
-  
-  def name=(dog_name)
-    @name = dog_name
   end
   
 end
